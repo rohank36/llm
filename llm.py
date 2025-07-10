c166f508040daf5f5e606b078b9e4f2a40b12b3a
Rohan Kanti <rohan.kanti@mail.mcgill.ca>
2025-07-09 23:17:46 -0400

    nothing

---
+++ llm.py
@@ -0,0 +1,11 @@
+class Head():
+    raise NotImplementedError
+
+class MultiHead():
+    raise NotImplementedError
+
+class FFNN():
+    raise NotImplementedError
+
+class Block():
+    raise NotImplementedError
---
+++ notes.md
@@ -0,0 +1,5 @@
+Deep dive into
+- backprop (understand loss.backward() )
+- transformer decoder for pre training 
+- instruct fine tuning via SFT
+- RL fine tuning 
