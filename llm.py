f2737aa1a1dd3736deb87c8c2c45b461cd056a17
Rohan Kanti <rohan.kanti@mail.mcgill.ca>
2025-07-12 18:42:05 -0400

    mha work cont

--- llm.py
+++ llm.py
@@ -1,15 +1,16 @@
-import math
+from math import sqrt
 import torch
 
-class Model:
-    B = 2 # batch size
-    T = 128 # sequence length i.e. chunk size
-    d = 128 # embedding dimension
-    hd = 64 # head dimension
-    nh = 6  # num heads
-    n_blocks = 3 
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+B = 2 # batch size
+T = 132 # sequence length i.e. chunk size
+d = 120 # embedding dimension
+nh = 6  # num heads
+assert d % nh == 0 
+hd = d // nh # head dimension
+n_blocks = 3 
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
+class Model:
     def __init__():
         pass
 
@@ -19,65 +20,48 @@ class Model:
     def generate():
         pass 
 
-
-
-class MultiHead:
+class MultiHeadAttention(torch.nn.Module):
     """
     X [B,T,d] --> Linear Layer[d,nh*3*hd] --> QKV [B,T,nh*3*hd]
-    reformat QKV --> qkv [B,nh,T,3,hd] 
+    view QKV --> qkv [B,nh,T,3,hd] 
     Q [B,nh,T,hd]
     K [B,nh,T,hd]
     V [B,nh,T,hd]
 
     Q [B,nh,T,hd] @ K.T [B,nh,hd,T] --> AM [B,nh,T,T]
     AM [B,nh,T,T] @ V [B,nh,T,hd] --> OUT [B,nh,T,hd]
+    OUT [B,nh,T,hd] concat head outputs --> OUT [B,T,nh*hd] 
+    OUT [B,T,nh*hd] @ LL [nh*hd,d] --> self attn out [B,T,d]
     
     """
     def __init__(self):
-        pass
-    
-class Head:
-    """
-    X [B,T,d] @ Qw [d,pd] = Q [B,T,pd]
-    X [B,T,d] @ Kw [d,pd] = K [B,T,pd]
-    X [B,T,d] @ Vw [d,d] = V [B,T,d]
+        super(MultiHeadAttention,self).__init__()
+        self.qkv_proj = torch.nn.Linear(d, nh * 3 * hd, bias=True) # Linear layer for QKV projections
+        self.ll = torch.nn.Linear(nh * hd, d, bias=True) # Linear layer for concatenated head output
+        self.to(device)
 
-    Q [B,T,pd] @ K.T [B,pd,T] = Attn_matrix [B,T,T]
-    Attn_matrix [B,T,T] @ V [B,T,d] = Head_output [B,T,d]
-
-
-
-    X [B,T,d] @ Qw [B,n_head,T*pd]
-    """
-
-    def __init__(self,d=Model.d, hd=Model.hd):
-        self.d = d
-        self.Qw = torch.randn(d,hd,requires_grad=True)
-        self.Kw = torch.randn(d,hd,requires_grad=True)
-        self.Vw = torch.randn(d,hd,requires_grad=True)
-
-        self.Qw = self.Qw.to(Model.device)
-        self.Kw = self.Kw.to(Model.device)
-        self.Vw = self.Vw.to(Model.device)
-
-
-    def single_head_masked_self_attention(self,Q,K,V,d):
-        attn_scores = (Q @ K.transpose(-2,-1)) / math.sqrt(d)
-        mask = torch.triu(torch.ones_like(attn_scores),diagonal=1).bool()
-        masked_attn_scores = attn_scores.masked_fill(mask,float('-inf'))
-        attn_weights = torch.nn.functional.softmax(masked_attn_scores,dim=-1)
-        head_output = attn_weights @ V
-        return head_output
-    
     def forward(self,X):
-        Q = X @ self.Qw
-        K = X @ self.Kw
-        V = X @ self.Vw
-        return self.single_head_masked_self_attention(Q,K,V,self.d)
-    
+        QKV = self.qkv_proj(X) # do QKV linear layer transformation
+        qkv = QKV.view(B,nh,T,3,hd) # group everything by batch -> head -> sequence -> QKV matrix
+        Q = qkv[:,:,:,0,:] # all Q matrices 
+        K_T = qkv[:,:,:,1,:].transpose(-2,-1) # all K matrices transposed
+        V = qkv[:,:,:,2,:] # all V matrices
+        AM = (Q @ K_T) / sqrt(d) # attention matrix
+        mask = torch.triu(torch.ones_like(AM),diagonal=1).bool()
+        masked_AM = AM.masked_fill(mask,float('-inf')) # masking upper triangle for causal self attention
+        attn_weights = torch.nn.functional.softmax(masked_AM,dim=-1) # softmax
+        out = attn_weights @ V
+        out_permute = out.permute(0,2,1,3) # [B,nh,T,hd] --> [B, T, nh, hd]
+        out_contig = out_permute.contiguous() # make contiguous in memory for view operation later
+        if not out_contig.is_contiguous():
+            raise Exception('Permuted tensor is not contiguous...')
+        out_heads_concat = out_contig.view(B,T,nh*hd) # [B, T, nh, hd] --> [B,T,nh*hd]
+        self_attention_out = self.ll(out_heads_concat) # [B,T,d]
+        return self_attention_out
 
-class MultiHead:
-    pass
-
-class Block: 
-    pass 
+if __name__ == "__main__":
+    mha = MultiHeadAttention()
+    X = torch.randn(B,T,d).to(device)
+    out = mha(X)
+    print(out.shape)
+    print(out)
